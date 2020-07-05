bb3ce1e9fb724b6f8f20e7a51ae09eed3ef3c996
Maxvyr <maxvy_@naver.com>
2020-07-05 18:42:45 +0200

    add boxDecoration background

--- lib/view/HomePage.dart
+++ lib/view/HomePage.dart
@@ -55,64 +55,79 @@ class _MyHomePageState extends State<MyHomePage> {
 
     return Scaffold(
       appBar: AppBarAuriga(titleTex: widget.title),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            // GreyfoxxTV Button
-            RaisedButtonLaunch(
-              function: () => setState(() {
-                _launchInWebViewWithJavaScript(greyfoxLinkTwitch);
-              }),
-              txt: "Go to GreyfoxxTV live",
-              color: blue,
-            ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              white,
+              grey.withOpacity(0.5),
+            ],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              // GreyfoxxTV Button
+              RaisedButtonLaunch(
+                function: () => setState(() {
+                  _launchInWebViewWithJavaScript(greyfoxLinkTwitch);
+                }),
+                txt: "Go to GreyfoxxTV live",
+                color: blue,
+              ),
 
-            // Nirusan Button
-            RaisedButtonLaunch(
-              function: () => setState(() {
-                _launched = _launchInWebViewWithJavaScript(nirusanLinkTwitch);
-              }),
-              txt: "Go to Nirusan Live",
-              color: red,
-            ),
+              // Nirusan Button
+              RaisedButtonLaunch(
+                function: () => setState(() {
+                  _launched = _launchInWebViewWithJavaScript(nirusanLinkTwitch);
+                }),
+                txt: "Go to Nirusan Live",
+                color: red,
+              ),
 
-            // KelrysTV Button
-            RaisedButtonLaunch(
-              function: () => setState(() {
-                _launched = _launchInWebViewWithJavaScript(kelrysTVLinkTwitch);
-              }),
-              txt: "Go to KelrysTV Live",
-              color: yellow,
-            ),
+              // KelrysTV Button
+              RaisedButtonLaunch(
+                function: () => setState(() {
+                  _launched =
+                      _launchInWebViewWithJavaScript(kelrysTVLinkTwitch);
+                }),
+                txt: "Go to KelrysTV Live",
+                color: yellow,
+              ),
 
-            // ReyexTv Button
-            RaisedButtonLaunch(
-              function: () => setState(() {
-                _launched = _launchInWebViewWithJavaScript(reyexTVLinkTwitch);
-              }),
-              txt: "Go to ReyexTV Live",
-              color: green,
-            ),
+              // ReyexTv Button
+              RaisedButtonLaunch(
+                function: () => setState(() {
+                  _launched = _launchInWebViewWithJavaScript(reyexTVLinkTwitch);
+                }),
+                txt: "Go to ReyexTV Live",
+                color: green,
+              ),
 
-            // Discord Button
-            RaisedButtonLaunch(
-              function: () => setState(() {
-                _launched = _launchInBrowser(discordLink);
-              }),
-              txt: "Discord",
-              color: purple,
-            ),
+              // Discord Button
+              RaisedButtonLaunch(
+                function: () => setState(() {
+                  _launched = _launchInBrowser(discordLink);
+                }),
+                txt: "Discord",
+                color: purple,
+              ),
 
-            // Youtube Button
-            RaisedButtonLaunch(
-              function: () => setState(() {
-                _launched = _launchInBrowser(youtubeLink);
-              }),
-              txt: "Youtube",
-              color: red,
-            ),
-          ],
+              // Youtube Button
+              RaisedButtonLaunch(
+                function: () => setState(() {
+                  _launched = _launchInBrowser(youtubeLink);
+                }),
+                txt: "Youtube",
+                color: red,
+              ),
+            ],
+          ),
         ),
       ),
     );
