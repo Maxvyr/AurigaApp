0a581af7e98d062b5cac463ebe967b88ec02dcf2
Maxvyr <maxvy_@naver.com>
2020-07-11 11:03:03 +0200

    add end drawer

--- lib/view/GreyfooxTV_Page.dart
+++ lib/view/GreyfooxTV_Page.dart
@@ -1,3 +1,5 @@
+import 'package:google_fonts/google_fonts.dart';
+
 import '../controller/Colors.dart';
 import '../controller/Constant.dart';
 import '../controller/Function_Launch_Web.dart';
@@ -21,6 +23,54 @@ class _GreyFoxxTVPageState extends State<GreyFoxxTVPage> {
         linkImg: greyfoxxLogo,
         titleText: "GreyFoxx TV",
         color: blue,
+      ),
+      endDrawer: Drawer(
+        child: Stack(
+          alignment: Alignment.topCenter,
+          children: <Widget>[
+            Container(
+              height: MediaQuery.of(context).size.height,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    blue,
+                    white,
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              top: MediaQuery.of(context).size.height * 0.1,
+              child: Text(
+                "Auriga TV",
+                style: GoogleFonts.mina(
+                  color: black,
+                  fontSize: 35.0,
+                  fontWeight: FontWeight.w900,
+                  shadows: <Shadow>[
+                    Shadow(
+                      offset: Offset(1.0, 1.0),
+                      blurRadius: 8.0,
+                      color: white,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              top: MediaQuery.of(context).size.height * 0.3,
+              child: IconButton(
+                  icon: Image.network("https://i.imgur.com/K8H0qxX.png"),
+                  onPressed: null),
+            ),
+            Positioned(
+              top: MediaQuery.of(context).size.height * 0.4,
+              child: IconButton(
+                  icon: Image.network("https://i.imgur.com/ouXb28u.png"),
+                  onPressed: null),
+            ),
+          ],
+        ),
       ),
       body: ContainerBackground(
         context: context,
--- lib/widget/AppBar_Auriga.dart
+++ lib/widget/AppBar_Auriga.dart
@@ -34,6 +34,5 @@ class AppBarAuriga extends AppBar {
             ),
           ),
           backgroundColor: color,
-          actions: <Widget>[],
         );
 }
